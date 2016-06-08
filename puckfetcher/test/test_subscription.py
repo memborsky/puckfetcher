41c1f823879392e62af7747d83f392bc9bf3a7d6
Andrew Michaud <andrewjmichaud@gmail.com>
2016-06-08 02:20:44 -0700

    oops

--- puckfetcher/test/test_subscription.py
+++ puckfetcher/test/test_subscription.py
@@ -178,7 +178,8 @@ class TestSubscription(object):
         for i in range(0, 4):
             _check_hi_contents(i, sub.directory)
 
-def _test_url_helper(self, given, name, expected_current, expected_provided):
+
+def _test_url_helper(given, name, expected_current, expected_provided):
     sub = SUB.Subscription(url=given, name=name, directory=TestSubscription.d)
     sub.get_feed()
 
