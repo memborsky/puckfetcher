b84ff1965d8289bc5bd908b974dd6540ff12c74a
Andrew Michaud <dev@mail.andrewmichaud.com>
2018-04-26 12:12:01 -0700

    Pin to latest version of dependencies

--- setup.py
+++ setup.py
@@ -17,13 +17,13 @@ INSTALL_REQUIRES = ["appdirs>=1.4.3, <2.0.0",
                     "feedparser>=5.2.1, <6.0.0",
                     "pyyaml>=3.12, <4.0.0",
                     "requests>=2.18.4, <3.0.0",
-                    "u-msgpack-python>=2.4.1, <3.0.0",
-                    "stagger>=1.0.0, <2.0.0",
-                    "drewtilities>=1.0.4, <2.0.0",
+                    "u-msgpack-python>=2.5.0, <3.0.0",
+                    "stagger>=1.0.1, <2.0.0",
+                    "drewtilities>=1.0.5, <2.0.0",
                    ]
 
-TEST_REQUIRES = ["coveralls>=1.2.0, <2.0.0",
-                 "pytest>=3.2.5, <4.0.0",
+TEST_REQUIRES = ["coveralls>=1.3.0, <2.0.0",
+                 "pytest>=3.5.0, <4.0.0",
                 ]
 
 setup(author="Andrew Michaud",
